1bf070d7b520d4545d2435b4dad76cfb22cf2bf1
ManojBadiger <66901015+ManojBadiger@users.noreply.github.com>
2020-08-31 15:19:46 +0530

    Add files via upload

---
+++ precache-manifest.70b6fa2b2ff2d877f08f36226a06020d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ce507549b946ba16d6a55b047984785",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dfdefbcfe0a03dbb20dd",
+    "url": "/static/css/main.d4ebf6d1.chunk.css"
+  },
+  {
+    "revision": "235f613daf567c0b9fd4",
+    "url": "/static/js/2.5973520a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.5973520a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfdefbcfe0a03dbb20dd",
+    "url": "/static/js/main.9f8ac356.chunk.js"
+  },
+  {
+    "revision": "aef0b2ee8c1ae484b595",
+    "url": "/static/js/runtime-main.949a55fe.js"
+  }
+]);
